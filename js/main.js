20c0625ba8c9e423f4b96bdfc8965a24ada308c1
maucou <maucou123@hotmail.com>
2024-02-23 00:14:20 -0300

    created the calculator logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-const display = document.querySelector(".display")
-const buttons = document.querySelectorAll(".button__calculadora")
-const specialChars = []
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('.button__calculadora')
+const specialChars = ['%', '*', '/', '-', '+', '=']
+let output = ''
 
 const calculate = (buttonValue) => {
-    console.log(buttonValue)
+
+    if(buttonValue === '=' && output !== '') {
+        output = eval(output.replace('%', '/100'))
+    } else if (buttonValue === 'ac') {
+        output = ''
+    } else if (buttonValue === 'del') {
+        output = output.toString().slice(0, -1)
+    } else {
+        if(output === '' && specialChars.includes(buttonValue)) return
+        output += buttonValue
+    }
+
+    display.value = output
 }
 
 buttons.forEach((button) => {
